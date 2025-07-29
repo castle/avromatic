dd4931a9ad90496537e7070de6a1b1ea8c1924fe
Sebastian Zuchmanski <sebastian.zuchmanski@castle.io>
2025-07-29 09:44:04 +0200

    Add support for iso8601 formatted string

--- lib/avromatic/model/types/timestamp_micros_type.rb
+++ lib/avromatic/model/types/timestamp_micros_type.rb
@@ -24,6 +24,8 @@ module Avromatic
         end
 
         def coerce_time(input)
+          # .iso8601 raises an error if the input is not a valid ISO 8601 string
+          # it is handled by the caller
           return Time.iso8601(input) if input.is_a?(String)
 
           # value is coerced to a local Time
